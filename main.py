e72a7801ba79c64bb1330eac08307cc059c1f603
Alla <allasamoilenko@gmx.de>
2024-11-02 18:18:10 +0200

    task2

--- main.py
+++ main.py
@@ -21,3 +21,37 @@ print(firstPerson.getInfo())
 print(secondPerson.getInfo())
 print(f"\nTotal number of Human objects created: {Human.countHuman()}")
 
+#task2
+class AreaCalculator:
+    count = 0
+
+    @staticmethod
+    def triangle_area(base, height):
+        AreaCalculator.count += 1
+        return 0.5 * base * height
+
+    @staticmethod
+    def rectangle_area(length, width):
+        AreaCalculator.count += 1
+        return length * width
+
+    @staticmethod
+    def square_area(side):
+        AreaCalculator.count += 1
+        return side ** 2
+
+    @staticmethod
+    def rhombus_area(diagonal1, diagonal2):
+        AreaCalculator.count += 1
+        return 0.5 * diagonal1 * diagonal2
+
+    @staticmethod
+    def get_calculation_count():
+        return AreaCalculator.count
+
+area_calculator = AreaCalculator()
+print("Triangle area (base=5, height=10):", area_calculator.triangle_area(5, 10))
+print("Rectangle area (length=4, width=6):", area_calculator.rectangle_area(4, 6))
+print("Square area (side=3):", area_calculator.square_area(3))
+print("Rhombus area (diagonal1=4, diagonal2=6):", area_calculator.rhombus_area(4, 6))
+print("Total area calculations:", AreaCalculator.get_calculation_count())
